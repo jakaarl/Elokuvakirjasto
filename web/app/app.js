5f377938826b967c992d16104fe02ee60d08febd
Jani Kaarela <jani.kaarela@gmail.com>
2015-08-27 00:01:09 +0300

    Yhdistetty hakusivu ja elokuvien listaus. Kaunisteltu yläpalkkia.

--- web/app/app.js
+++ web/app/app.js
@@ -7,14 +7,13 @@ Elokuvakirjasto.config(function ($routeProvider) {
             .when('/register', { controller: 'UserManagement', templateUrl: 'app/views/register.html' })
             .when('/movies', { controller: 'ListMovies', templateUrl: 'app/views/movies.html' })
             .when('/movies/new', { controller: 'AddMovie', templateUrl: 'app/views/add_movie.html' })
-            .when('/movies/find', { controller: 'FindMovies', templateUrl: 'app/views/find_movies.html'})
             .when('/movies/:key', { controller: 'ViewMovie', templateUrl: 'app/views/view_movie.html' })
             .when('/movies/:key/edit', { controller: 'EditMovie', templateUrl: 'app/views/edit_movie.html' });
 }).config(['$httpProvider', function ($httpProvider) {
     delete $httpProvider.defaults.headers.common["X-Requested-With"];
-}]).run(function($rootScope, AuthenticationService) {   
+}]).run(function($rootScope, AuthenticationService) {
     $rootScope.loggedInUser = AuthenticationService.getLoggedInUser();
     $rootScope.logOut = function () {
-        AuthenticationService.logUserOut();    
+        AuthenticationService.logUserOut();
     };
 });
